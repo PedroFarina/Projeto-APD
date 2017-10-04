798dc4f97c1703e040a5b4a98c00a031d1005cef
leolongato <31939705+leolongato@users.noreply.github.com>
2017-10-04 19:55:52 -0300

    Adicionando interface usuário

---
+++ Implementação/gui/interface_usuario.py
@@ -0,0 +1,33 @@
+from gui import menu_usuario
+from gui import usuario
+
+from gui import menu_filme
+from gui import filme
+
+def iniciar_modulos():
+    usuario.iniciar_usuarios()
+    filme.iniciar_filmes()
+
+def mostrar_menu():    
+    iniciar_modulos()
+    
+    rodando = True
+    
+    menu = ("\n----------------\n"+
+             "(1) Menu Filme \n" +
+             "(2) Menu Usuario \n" +
+             "(0) Sair\n"+
+            "----------------")
+    while rodando:
+        print(menu)
+        op = input("Digite sua escolha:")
+        
+        if op == "0":
+            rodando = False
+        elif op == "1":
+            menu_usuario.mostrar_menu()
+        elif op == "2":
+            menu_usuario.mostrar_menu()
+        else:
+            print("Escolha inválida!")
+        
